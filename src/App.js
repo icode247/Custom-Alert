a6f7c8779ca4f3458b8eb8a2ed2ae57f5dd0ac17
icode247 <zionekekenta@gmail.com>
2022-03-02 23:32:30 +0100

    Done!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Alert type="error" message="Error" />
+      <Alert type="success">
+        <p>Success message</p>
+      </Alert>
+      <Alert type="primary">
+        <h4>primary message</h4>
+      </Alert>
+      <Alert type="secondary">
+        <span>secondary message</span>
+      </Alert>
     </div>
   );
 }
